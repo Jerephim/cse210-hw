7a054af99d9bfe20c2d0de9c8784687a664d9ec4
Jerephim <joh15057@byui.edu>
2023-02-25 20:11:24 -0700

    good start on the assignment

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,148 @@
 using System;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            Console.WriteLine("Select an activity:\n1. Breathing\n2. Reflection\n3. Listing");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    BreathingActivity();
+                    break;
+                case 2:
+                    ReflectionActivity();
+                    break;
+                case 3:
+                    ListingActivity();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+    }
+    static int GetDuration()
+    {
+        int duration;
+        while (true)
+        {
+            Console.Write("Enter the desired duration in seconds: ");
+            if (int.TryParse(Console.ReadLine(), out duration) && duration > 0)
+            {
+                return duration;
+            }
+            Console.WriteLine("Invalid input. Please enter a positive integer value.");
+        }
+    }
+
+    static void Delay(int seconds)
+    {
+        int milliseconds = seconds * 1000;
+        Thread.Sleep(milliseconds);
+    }
+    static void BreathingActivity()
+    {
+        Console.WriteLine("Breathing Activity");
+        Console.WriteLine("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        int duration = GetDuration();
+
+        Console.WriteLine("Get ready to begin...");
+        Delay(3);
+
+        Console.WriteLine("Breathe in...");
+        Delay(2);
+
+        Console.WriteLine("Breathe out...");
+        Delay(2);
+
+        while (duration > 0)
+        {
+            Console.WriteLine("Breathe in...");
+            Delay(2);
+
+            Console.WriteLine("Breathe out...");
+            Delay(2);
+
+            duration -= 4;
+        }
+
+        Console.WriteLine("Good job! You have completed the Breathing Activity for {0} seconds.", duration + 4);
+        Delay(3);
+    }
+    static void Spinner()
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        int spinnerIndex = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(spinner[spinnerIndex % spinner.Length] + " ");
+            spinnerIndex++;
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+        }
+        Console.WriteLine();
+    }
+    static void ReflectionActivity()
+    {
+        Console.WriteLine("Reflection Activity");
+        Console.WriteLine("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        int duration = GetDuration();
+
+        Console.WriteLine("Get ready to begin...");
+        Delay(3);
+
+        Random random = new Random();
+        string[] prompts = {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+        string[] questions = {
+                "Why was this experience meaningful to you?",
+                "Have you ever done anything like this before?",
+                "How did you get started?",
+                "How did you feel when it was complete?",
+                "What made this time different than other times when you were not as successful?",
+                "What is your favorite thing about this experience?",
+                "What could you learn from this experience that applies to other situations?",
+                "What did you learn about yourself through this experience?",
+                "How can you keep this experience in mind in the future?"
+            };
+        while (duration > 0)
+        {
+            string prompt = prompts[random.Next(prompts.Length)];
+            Console.WriteLine(prompt);
+            Delay(2);
+
+            
+
+            foreach (string question in questions)
+            {
+                Console.Write("  {0}... ", question);
+                Spinner();
+                Delay(2);
+            }
+
+            duration -= (prompts.Length * (questions.Length + 1)) * 2;
+        }
+
+        Console.WriteLine("Good job! You have completed the Reflection Activity for {0} seconds.", duration + (prompts.Length * (questions.Length + 1)) * 2);
+        Delay(3);
+    }
+
+    static void ListingActivity()
+    {
+        Console.WriteLine("Listing Activity");
+        Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        int duration = GetDuration();
+
+        Console.WriteLine("Gotta finish from here");
     }
 }
